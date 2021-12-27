b9806c5cf6cd22b7bff1ceac1a9a158afb55de91
liraRahmawati17 <lira_180@smkassalaambandung.sch.id>
2021-12-28 00:02:54 +0700

    baru

---
+++ database/seeders/userSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $admin = Role::create([
+            'name' => 'admin',
+            'Display_name' => 'User Administrator'
+        ]);
+
+        $pengguna = Role::create([
+            'name' => 'pengguna',
+            'display_name' => 'user Biasa'
+        ]);
+
+        $user = new User();
+        $user->name = 'Lira rahmawati';
+        $user->email = 'admin@gmail.com';
+        $user->password = Hash::make('12345678');
+        $user->save();
+
+        $pengunjung = new User();
+        $pengunjung->name = 'pengunjung';
+        $pengunjung->email = 'pengunjung@gmail.com';
+        $pengunjung->password = Hash::make('12345678');
+        $pengunjung->save();
+
+        $user->attachRole($admin);
+        $pengunjung->attachRole($pengguna);
+
+    }
+}
